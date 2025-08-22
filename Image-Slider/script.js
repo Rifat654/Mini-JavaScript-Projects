de1975eb46d04268e2fba1ec1d57286f454c24fc
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-08-23 01:14:25 +0600

    added

---
+++ Image-Slider/script.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector(".slider");
+
+
+async function fetchImages() {
+    try{
+
+        const response = await fetch("https://picsum.photos/v2/list?page=2&limit=10", {
+            method: "GET"
+        });
+
+        const imgList = await response.json();
+        if (imgList &&  imgList.length > 0) displayImage(imgList)
+        console.log(imgList);
+        
+
+    }catch(error){
+        console.log(`Error fetching images: ${error}`);
+        
+    }
+}
+
+
+function displayImage( getImgList) {
+
+    slider.style.width = "500px";
+    slider.style.height = "300px";
+    slider.innerHTML = getImgList.map((item) => `
+        <div class="slide">
+        
+        <img  src="${item.download_url}" alt="Image by ${item.author}" />
+        </div>
+        `)
+}
+
+
+fetchImages();
