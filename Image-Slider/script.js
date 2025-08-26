f9e5f542b1be6851147a099273e86f7b32bc88a5
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-08-26 11:33:55 +0600

    added slider logic

--- Image-Slider/script.js
+++ Image-Slider/script.js
@@ -3,5 +3,32 @@ const slides = document.querySelectorAll(".slides");
 let counter = 0;
 
 slides.forEach((slide, index) =>{
-    slide.style.left =`${index * 100}%`;
+    slide.style.left =`${index* 100}%`;
 })
+
+
+const slideImage = () =>{
+    slides.forEach((slide) =>{
+        slide.style.transform = `translateX(-${counter* 100}%)`;
+    })
+}
+
+
+// sliding functions //
+const goPrev = () =>{
+    counter --
+    slideImage()
+  
+    
+}
+
+
+
+
+const goNext = () =>{
+    counter ++
+    slideImage()
+
+}
+
+
