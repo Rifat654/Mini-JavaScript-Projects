d14a7da4000726478be617caf325a40d695aecba
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-09-30 10:34:40 +0600

    adding js logics for todo adding

--- Todo-App/app.js
+++ Todo-App/app.js
@@ -1,1 +1,48 @@
-console.log("Welcome to Todo App");
+let task = [];
+// get elements
+let btn = document.getElementById("newTask");
+let input = document.getElementById("input");
+let list = document.getElementById("taskList");
+
+// event listener for add button
+btn.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    addTask();
+})
+
+
+// function for add task
+const addTask = () =>{
+
+    const text = input.value.trim();
+
+        if(text){
+            task.push({text: text, done:false});
+
+            updateTaskList()
+        }
+     
+        
+}
+
+// function to update task list
+const updateTaskList = () =>{
+    list.innerHTML = "";
+
+    task.forEach(taskItem =>{
+        const li = document.createElement("li");
+        list.innerHTML = `
+            <div class="taskItem"> 
+               <div class="taskText">
+                <p class="pp"> ${taskItem.text} </p>
+               </div>
+               <div>
+                <button class="btn "> <i class="fa-solid fa-trash"></i> </button>
+               </div>
+                
+            </div>
+        `
+        list.appendChild(li);
+    })
+}
