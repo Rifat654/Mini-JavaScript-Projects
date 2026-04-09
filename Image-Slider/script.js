c2a7f52f1bb24bb88594b4b2faeb59a35579aba6
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2026-04-09 18:47:04 +0600

    added

--- Image-Slider/script.js
+++ Image-Slider/script.js
@@ -2,35 +2,33 @@ const slides = document.querySelectorAll(".slides");
 
 let counter = 0;
 
-slides.forEach((slide, index) =>{
-    slide.style.left =`${index* 100}%`;
+slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`;
 })
 
 
-const slideImage = () =>{
-    slides.forEach((slide) =>{
-        slide.style.transform = `translateX(-${counter* 100}%)`;
+const slideImage = () => {
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
     })
 }
 
 
 // sliding functions //
-const goPrev = () =>{
+const goPrev = () => {
     //  counter = (counter - 1 + slides.length) % slides.length;
-    counter  = counter === 0 ? slides.length -1 : counter - 1;
+    counter = counter === 0 ? slides.length - 1 : counter - 1;
     slideImage()
-  
-    
+
+
 }
 
 
 
 
-const goNext = () =>{
+const goNext = () => {
     //   counter = (counter + 1) % slides.length;
-    counter  = counter === slides.length  -1 ? 0 : counter + 1;
+    counter = counter === slides.length - 1 ? 0 : counter + 1;
     slideImage()
 
 }
-
-
